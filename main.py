06b50a80077b71f785711566e78145a5fd746c29
nhusna01 <nrlhsn1310@gmail.com>
2026-06-09 09:15:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ st.markdown(
 # 📂 Sidebar Navigation
 # ==============================
 # ✅ Add UMK logo at the top of sidebar
-st.sidebar.image("images/UMK_logo1.png", width=100)
+st.sidebar.image("images/UMK_logo1.png", width=250)
 
 st.sidebar.title("Navigation")
 
