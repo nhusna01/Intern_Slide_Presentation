87b3a84a850dc41433c3c47c42d92551ac0b64cb
nhusna01 <nrlhsn1310@gmail.com>
2026-06-25 12:31:20 +0800

    Update myself.py

--- pages/myself.py
+++ pages/myself.py
@@ -1,5 +1,13 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
 
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+    
 def myself_page():
     # Stylized title
     st.markdown("""
